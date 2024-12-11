7b191dd7fe78465cb3f76779feecdb7168cffd34
Hadi Khan <hadi70037@gmail.com>
2024-12-10 20:13:31 -0500

    class for e-transfer

---
+++ src_codes/etransfer.js
@@ -0,0 +1,9 @@
+class Etransfer {
+    constructor(sender, recipent, amount, securityQuestion, securityQuestionAnswer) {
+        this.sender = sender;
+        this.recipent = recipent;
+        this.amount = amount;
+        this.securityQuestion = securityQuestion;
+        this.securityQuestionAnswer = securityQuestionAnswer;
+    }
+}
