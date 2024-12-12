a0354e80e2f56db4c8d3a0925333bdc08a9785ad
Hadi Khan <hadi70037@gmail.com>
2024-12-11 23:04:09 -0500

    Update etransfer.js

--- src_codes/etransfer.js
+++ src_codes/etransfer.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 class Etransfer {
     constructor(sender, recipent, amount, securityQuestion, securityQuestionAnswer) {
         this.sender = sender;
@@ -10,7 +11,7 @@ class Etransfer {
        // Method to send E-transfer
     sendETransfer(user) {
         if (user.balance >= this.amount) {
-            user.balance = this.amount;
+            user.balance -= this.amount;
             const tranfer = {
                 sender: this.sender,
                 recipent: this.recipent,
@@ -19,8 +20,22 @@ class Etransfer {
                 securityQuestionAnswer: this.securityQuestionAnswer
 
             };
-            
 
-        }
+            let data = [];
+            try {
+                data = JSON.parse(fs.readFileSync('transfer.json', 'utf8'));
+            } catch (error) {
+                console.log('No existing transfer data found');
+            }
+            
+            data.push(tranfer);
+            fs.writeFileSync('transfer,JSON', JSON.stringify(data, null));
+
+            console.log(`E-Transfer of $${this.amount} transfered to ${this.recipent}.`);
+             return true
+            } else {
+                console.log("Insuffent Funds");
+                return false;
+            }
     }
 }
