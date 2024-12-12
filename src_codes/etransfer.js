c314ae1b8c7643d14691d3f50ffb530229a95d24
Hadi Khan <hadi70037@gmail.com>
2024-12-11 22:48:01 -0500

    added method to send E-Transfer

--- src_codes/etransfer.js
+++ src_codes/etransfer.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 class Etransfer {
     constructor(sender, recipent, amount, securityQuestion, securityQuestionAnswer) {
         this.sender = sender;
@@ -5,5 +6,21 @@ class Etransfer {
         this.amount = amount;
         this.securityQuestion = securityQuestion;
         this.securityQuestionAnswer = securityQuestionAnswer;
+    }
+       // Method to send E-transfer
+    sendETransfer(user) {
+        if (user.balance >= this.amount) {
+            user.balance = this.amount;
+            const tranfer = {
+                sender: this.sender,
+                recipent: this.recipent,
+                amount: this.amount,
+                securityQuestion: this.securityQuestion,
+                securityQuestionAnswer: this.securityQuestionAnswer
+
+            };
+            
+
+        }
     }
 }
