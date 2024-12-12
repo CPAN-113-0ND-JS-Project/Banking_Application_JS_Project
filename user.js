d6344a8a4a832ce31622a356fd83ee5c9776a63b
AndreSurnam <tolentino.andre.edups@gmail.com>
2024-12-12 14:38:40 -0500

    Finished with user.js. 1. Decided to delete src_codes folder and moved all files inside it to the main directory (will do the same in main). 2. Added type: commonjs to the package json since we're using require functions. 3. Installed prompt-sync to use prompt function (will do the same in main). 4. user.js has User class and inside has authentication, withdraw, deposit and viewBalance functions. The User contructor has 5 objects: email, pin, balance, isLocked and failedAttempts. 5. Authentication function has three if statements: one tracks if isLocked is true, it will print out a message that user's account has been locked out and return false. The function checks if the account has locked out first then prompts the user to enter a PIN. Second if statement tracks if the PIN input by user is the same PIN it has for the account. If they fail, print out an error message, then plus 1 to failedAttempts. When failedAttempts has reached 3 failed attempts, the account is locked out then prints out an error message. If the input was correct, set back failedAttempts to 0 and return true. 6. View balance function is simple. Nothing special, just prints out a message their balance with the current balance they have on. 7. Deposit function prompts the user first on how much they want to deposit. The value must be always be above 0, else it prints out an error message then returns. By how many they deposited, it will add with current balance they had then prints out a successful message. 8. Lastly, withdraw function. Prompts the user first, then if the input is 0 or less, prints out an error message then returns  to prompt user again(in theory, same goes for deposit function). If the user input is greater than user's current balance, same thing happens with withdrawing less than or equal to 0. If it's succesful, balance will get subtracted by the amount of user's input, then prints a successful message. 8. Haven't added the users data json file yet. Will do it in main.

---
+++ user.js
@@ -0,0 +1,81 @@
+const prompt = require('prompt-sync')({ sigint: true });
+
+class User {
+    constructor(email, pin, balance = 0) {
+        this.email = email; 
+        this.pin = pin;
+        this.balance = balance;
+        this.failedAttempts = 0;
+        this.isLocked = false;
+    }
+
+    authenticate() {
+
+        if (this.isLocked) {
+            console.log();
+            console.error("Your account is locked due to too many failed attempts.");
+            return false;
+        }
+
+        var promptInputPin = parseInt(prompt("Enter your PIN: "));
+
+        if (this.pin !== promptInputPin) {
+            this.failedAttempts += 1;
+            console.log();
+            console.error("Incorrect PIN input. Please try again.");
+            if (this.failedAttempts >= 3) {
+                this.isLocked = true; 
+                console.log();
+                console.error("You have failed too many attempts... User has been locked out. Your session has been forcibly stopped.");
+            }
+            return false;
+        }
+
+        this.failedAttempts = 0;
+        return true;
+
+    }
+
+    viewBalance() {
+
+        console.log();
+        console.log(`Your account's balance is $${this.balance}`);
+
+    }
+
+    deposit() {
+
+        var promptDepBal = parseInt(prompt("Deposit: $"))
+
+        if (promptDepBal <= 0) {
+            console.log();
+            console.error("Deposit amount must be above $0.");
+            return;
+        }
+        this.balance += promptDepBal;
+        console.log();
+        console.log(`Successfully deposited $${promptDepBal}. Your new balance is $${this.balance}`);
+    }
+
+    withdraw() {
+
+        var promptWithBal = parseInt(prompt("Withdraw: $"))
+
+        if (promptWithBal <= 0) {
+            console.log();
+            console.error("Withdrawal amount must be above $0.");
+            return;
+        }
+        if (this.balance < promptWithBal) {
+            console.log();
+            console.error(`Your account's balance has insufficient funds to withdraw $${promptWithBal}.`);
+            return;
+        }
+        this.balance -= promptWithBal;
+        console.log();
+        console.log(`Successfully withdrew $${promptWithBal}. Your new balance is $${this.balance}`);
+    }
+    
+}
+
+module.exports = User;
